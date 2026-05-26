5b6a4e820503f2e43ed74899964e2ac670de7931
wildgrowthmedia <271601290+wildgrowthmedia@users.noreply.github.com>
2026-05-26 18:26:41 -0400

    Implement autoplay for Vimeo videos on hover
    Added functionality to autoplay Vimeo videos on mouse enter and reset on mouse leave.

--- script.js
+++ script.js
@@ -4,9 +4,29 @@ const modalVideo = document.getElementById("modalVideo");
 const closeModal = document.getElementById("closeModal");
 
 projects.forEach((project) => {
+  const iframe = project.querySelector("iframe");
+  const player = new Vimeo.Player(iframe);
+  let loopTimer;
+
+  project.addEventListener("mouseenter", () => {
+    player.setCurrentTime(0);
+    player.play();
+
+    loopTimer = setInterval(() => {
+      player.setCurrentTime(0);
+      player.play();
+    }, 6000);
+  });
+
+  project.addEventListener("mouseleave", () => {
+    clearInterval(loopTimer);
+    player.pause();
+    player.setCurrentTime(0);
+  });
+
   project.addEventListener("click", () => {
     const videoUrl = project.dataset.vimeo;
-    modalVideo.src = `${videoUrl}&autoplay=1`;
+    modalVideo.src = `${videoUrl}?autoplay=1`;
     modal.classList.add("is-open");
   });
 });
